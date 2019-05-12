088fcbacaa1ed4489190ef7a8baf554e403a607f
Moumita Saha <sahamoumita318@gmail.com>
2019-05-12 21:58:42 +0530

    navbar is created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Navigation from './Navigation.js'
-import MainContent from './MainContent.js'
-import Footer from './Footer.js'
+import Navigation from './Navigation.js';
+import MainContent from './MainContent.js';
+import Footer from './Footer.js';
+import logo from './logo.png';
 
 class App extends React.Component {
 	render(){
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import logo from './logo.png';
+import symbol from './symbol.png';
 
 class Navigation extends React.Component{
 	render(){
 		return(
-	
-	<h1>mouthyjfukryukyruk</h1>
+			<div>	
+		
+<ul>
+  <li><img src={symbol} alt="logo2" className="logo2"/></li>
+  <li><a href="#claim">Claims</a></li>
+  <li><a href="#news">Login</a></li>
+  <li><a href="#contact">ContactUs</a></li>
+  <li><a href="#about">About</a></li>
+  <img src={logo} alt="Logo" className="logo" />
+  
+
+</ul>
+</div>
 )
 
 
